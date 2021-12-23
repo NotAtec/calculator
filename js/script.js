0c6680de8d33cd485d54c6acf46cd058d9dbe3ae
Jeroen B <jeroenb04@live.com>
2021-12-23 17:06:56 +0100

    Setup handling for decimal & Backspace

--- js/script.js
+++ js/script.js
@@ -1,3 +1,62 @@
+let display = '0';
+let decimal = false;
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', buttonHandler);
+});
+
+function buttonHandler(e) {
+    switch (e.target.id) {
+        case 'decimal':
+            decimalHandler();
+            break;
+        case 'delete':
+            backspace();
+            break;
+        case 'reset':
+            resetCalculator();
+        default:
+            if (e.target.classList.contains('operator')) {
+                operatorHandler(e.target.id);
+            } else {
+                numberHandler(e.target.id);
+            }  
+    }
+}
+
+function numberHandler(id) {
+    console.log(id);
+}
+
+function operatorHandler(id) {
+    console.log(id);
+}
+
+function decimalHandler() {
+    if (decimal) {
+        return;
+    } else {
+        decimal = true;
+        display += '.';
+    }
+    displayString();
+}
+
+function backspace() {
+    displayArray = display.split('');
+    if (displayArray.pop() == '.') {
+        decimal = false;
+    }
+    display = displayArray.join('');
+    displayString();
+}
+
+function displayString() {
+    screen = document.getElementById('numbers');
+    screen.innerText = display;
+}
+
 function add(a, b) {
     return +a + +b;
 }
